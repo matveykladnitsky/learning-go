8e6ceed0360ce064fcf3aa98d7d724db0fe9ea22
Danil Kladnitsky <danyakladnitsky@gmail.com>
2024-12-14 11:34:27 +0300

    fix: update tests and ci

--- basic/helloworld/main_test.go
+++ basic/helloworld/main_test.go
@@ -5,7 +5,7 @@ import "testing"
 
 func TestHelloWorld(t *testing.T) {
 	repeated := sayHelloWorld()
-	expected := "Hello World!"
+	expected := "Hello, World!"
 
 	if repeated != expected {
 		t.Errorf("Ожидаемое значение: %q. Получено: %q", expected, repeated)
