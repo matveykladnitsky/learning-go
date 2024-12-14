811fd4cdd50a58e7778d40cdf2cd8e76bfec630d
Danil Kladnitsky <danyakladnitsky@gmail.com>
2024-12-14 11:54:04 +0300

    fix: lint test

--- basic/helloworld/main_test.go
+++ basic/helloworld/main_test.go
@@ -2,12 +2,11 @@ package helloworld
 
 import "testing"
 
-
 func TestHelloWorld(t *testing.T) {
 	repeated := sayHelloWorld()
-	expected := "Hello, World!"  
-		   
+	expected := "Hello, World!"
+
 	if repeated != expected {
-		  t.Errorf("Ожидаемое значение: %q. Получено: %q", expected, repeated)
+		t.Errorf("Ожидаемое значение: %q. Получено: %q", expected, repeated)
 	}
 }
